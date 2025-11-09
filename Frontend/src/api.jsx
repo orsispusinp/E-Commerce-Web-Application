a7396bbcf9b4e1046a95ad39916bc1ed91b4fee3
RAJEEV SUTRAKAR <sutrakarrajeev22@gmail.com>
2025-11-09 11:19:58 +0530

    fix api

--- Frontend/src/api.jsx
+++ Frontend/src/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://e-commerce-backend-zuxw.onrender.com", // change after deployment
+  baseURL: "https://e-commerce-backend-zuxw.onrender.com/api", // change after deployment
 });
 
 // Add token to headers automatically
